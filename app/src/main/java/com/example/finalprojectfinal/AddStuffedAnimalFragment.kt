5b08f3ccbe88a2082d8f933487ad720c341737ae
rustytoe69 <rustytoe5@icloud.com>
2024-05-23 12:57:59 -0400

    Continued working on fragments UI (finshed AddStuffedAnimalFragment UI and setup the spinner options)

--- app/src/main/java/com/example/finalprojectfinal/AddStuffedAnimalFragment.kt
+++ app/src/main/java/com/example/finalprojectfinal/AddStuffedAnimalFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.finalprojectfinal.databinding.FragmentAddStuffedAnimalBinding
 import com.example.finalprojectfinal.databinding.FragmentMainBinding
 import com.example.finalprojectfinal.databinding.FragmentStuffedAnimalListBinding
@@ -21,5 +22,12 @@ class AddStuffedAnimalFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding= FragmentAddStuffedAnimalBinding.inflate(inflater, container, false)
         val rootView = binding.root
-        return rootView    }
+
+        //for setting up spinner
+        val stuffedAnimalArrayAdapter = ArrayAdapter.createFromResource(requireActivity(),
+            R.array.stuffed_animal_types,android.R.layout.simple_spinner_item)
+        stuffedAnimalArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.animalTypeSpinner.adapter=stuffedAnimalArrayAdapter
+        return rootView
+    }
 }
