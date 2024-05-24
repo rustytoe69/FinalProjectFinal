52753c160ec6f8c594bab3a28950ad8577dd23de
rustytoe69 <rustytoe5@gmail.com>
2024-05-23 20:51:25 -0400

    finished fragment UIs

--- app/src/main/java/com/example/finalprojectfinal/StuffedAnimalDetailsFragment.kt
+++ app/src/main/java/com/example/finalprojectfinal/StuffedAnimalDetailsFragment.kt
@@ -1,7 +1,25 @@
 package com.example.finalprojectfinal
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.finalprojectfinal.databinding.FragmentMainBinding
+import com.example.finalprojectfinal.databinding.FragmentStuffedAnimalDetailsBinding
 
+class StuffedAnimalDetailsFragment : Fragment() {
+
+    private var _binding: FragmentStuffedAnimalDetailsBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding= FragmentStuffedAnimalDetailsBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+        return rootView    }
+}
