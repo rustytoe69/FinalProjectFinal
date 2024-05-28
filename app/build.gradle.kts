fbd686a056fc3fb6551b00238de19da4ba92ab76
rustytoe69 <rustytoe5@icloud.com>
2024-05-28 12:52:57 -0400

    Continued working on the recycler view (finished the model class (step1), created the recycler view view in stuffedAnimalListFragment (step3), created the list item layout file (4a))

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     //for recyclerview
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     //set up nav graph + navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
--- app/src/main/java/com/example/finalprojectfinal/StuffedAnimal.kt
+++ app/src/main/java/com/example/finalprojectfinal/StuffedAnimal.kt
@@ -1,20 +1,35 @@
 package com.example.finalprojectfinal
+
 data class StuffedAnimal(
     var animalType: String,
     val name: String,
-    val birthdate: String)
+    val birthdate: String
+) {
 
 
 //lateinit var animalType:String
 //animalType = animalType
 
-val imageResourceId:Int
-    get()=
-        if(animalType.equals("Cat"))R.drawable.cat
-        else if(animalType.equals("Dog"))R.drawable.dog
-        else if(animalType.equals("Dragon"))R.drawable.dragon
-        else if(animalType.equals("Horse"))R.drawable.horse
-        else if(animalType.equals("Penguin"))R.drawable.penguin
-        else if(animalType.equals("Bear"))R.drawable.bear
-        else if(animalType.equals("Other"))R.drawable.other
-        else R.drawable.other
+    fun returnImageResourceId(): Int {
+    if (animalType.equals("Cat")) return R.drawable.cat
+    else if (animalType.equals("Dog")) return R.drawable.dog
+    else if (animalType.equals("Dragon")) return R.drawable.dragon
+    else if (animalType.equals("Horse")) return R.drawable.horse
+    else if (animalType.equals("Penguin")) return R.drawable.penguin
+    else if (animalType.equals("Bear")) return R.drawable.bear
+    else if (animalType.equals("Other")) return R.drawable.other
+    else return R.drawable.other
+    }
+
+//val imageResourceId:Int
+//    get() {
+//        if (animalType.equals("Cat")) R.drawable.cat
+//        else if (animalType.equals("Dog")) R.drawable.dog
+//        else if (animalType.equals("Dragon")) R.drawable.dragon
+//        else if (animalType.equals("Horse")) R.drawable.horse
+//        else if (animalType.equals("Penguin")) R.drawable.penguin
+//        else if (animalType.equals("Bear")) R.drawable.bear
+//        else if (animalType.equals("Other")) R.drawable.other
+//        else R.drawable.other
+//    }
+}
